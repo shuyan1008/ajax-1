6b06f20c4a4dd6291a39286b871b33d10bdaffe9
shuyan <3446498965@qq.com>
2020-06-16 19:01:56 +0800

    使用server.js创建html/css/js/xml/JSON

---
+++ public/main.js
@@ -0,0 +1,71 @@
+console.log(`我是main.js2`);
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/5.json');
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const object = JSON.parse(request.response);
+      myName.textContent = object.name;
+    }
+  };
+  request.send();
+};
+getHtml.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/3.html'); //readyState =1
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const div = document.createElement('div');
+        div.innerHTML = request.response;
+        document.body.appendChild(div);
+      } else {
+        alert('加载失败');
+      }
+    }
+  };
+
+  request.send(); //readyState =2
+};
+getXml.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/4.xml');
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName('warning')[0].textContent;
+      console.log(text.trim());
+    }
+  };
+  request.send();
+};
+getCss.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/style.css');
+  //method要使用的HTTP方法，比如「GET」、「POST」、「PUT」、「DELETE」
+  //url一个DOMString表示要向其发送请求的URL。
+  request.onload = () => {
+    //创建style标签
+    const style = document.createElement(`style`);
+    //填写style内容
+    style.innerHTML = request.response;
+    //插到头里面
+    document.head.appendChild(style);
+  };
+  request.onerror = () => {
+    console.log('失败了');
+  };
+  request.send();
+};
+getJs.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open('GET', '/2.js');
+  request.onload = () => {
+    const script = document.createElement('script');
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  };
+  request.onerror = () => {};
+  request.send();
+};
